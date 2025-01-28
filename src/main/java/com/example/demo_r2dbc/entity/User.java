3644be8b5499a137aaa278ac99ca8a6a325f92f5
indrawijayakusuma <indrawk56@gmail.com>
2025-01-28 20:44:01 +0700

    fix connection to db

--- src/main/java/com/example/demo_r2dbc/entity/User.java
+++ src/main/java/com/example/demo_r2dbc/entity/User.java
@@ -3,10 +3,14 @@ package com.example.demo_r2dbc.entity;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Setter @Getter
+@Table("users")
 public class User {
+    @Id
     private Long id;
     private String name;
     private String email;
